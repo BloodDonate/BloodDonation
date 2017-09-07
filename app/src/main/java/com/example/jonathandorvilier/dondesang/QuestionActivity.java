aa58a87b61f7d5f82e61d58013dfed75953cb802
IngDjason <djason_sylvaince@yahoo.fr>
2017-09-07 17:57:51 -0400

    Questionnaire
    QUESTIONNAIRE SUR LE DON DE SANG

---
+++ app/src/main/java/com/example/jonathandorvilier/dondesang/QuestionActivity.java
@@ -0,0 +1,89 @@
+package com.example.jonathandorvilier.dondesang;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    private RadioGroup q1,q2,q3,q4,q5,q6,q7;
+    private RadioButton radioButton1,radioButton2,radioButton3,radioButton4,radioButton5,radioButton6,radioButton7;
+    private Button btnVerifier;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Questionnaire");
+
+        btnVerifier = (Button) findViewById(R.id.btnVerifier);
+
+        q1 = (RadioGroup) findViewById(R.id.q1);
+        q2 = (RadioGroup) findViewById(R.id.q2);
+        q3 = (RadioGroup) findViewById(R.id.q3);
+        q4 = (RadioGroup) findViewById(R.id.q4);
+        q5 = (RadioGroup) findViewById(R.id.q5);
+        q6 = (RadioGroup) findViewById(R.id.q6);
+        q7 = (RadioGroup) findViewById(R.id.q7);
+
+        btnVerifier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int selected1 = q1.getCheckedRadioButtonId();
+                int selected2 = q2.getCheckedRadioButtonId();
+                int selected3 = q3.getCheckedRadioButtonId();
+                int selected4 = q4.getCheckedRadioButtonId();
+                int selected5 = q5.getCheckedRadioButtonId();
+                int selected6 = q6.getCheckedRadioButtonId();
+                int selected7 = q7.getCheckedRadioButtonId();
+
+                radioButton1 = (RadioButton) findViewById(selected1);
+                radioButton2 = (RadioButton) findViewById(selected2);
+                radioButton3 = (RadioButton) findViewById(selected3);
+                radioButton4 = (RadioButton) findViewById(selected4);
+                radioButton5 = (RadioButton) findViewById(selected5);
+                radioButton6 = (RadioButton) findViewById(selected6);
+                radioButton7 = (RadioButton) findViewById(selected7);
+
+                if(q1.getCheckedRadioButtonId() != -1 && q2.getCheckedRadioButtonId() != -1 && q3.getCheckedRadioButtonId() != -1 && q4.getCheckedRadioButtonId() != -1 && q5.getCheckedRadioButtonId() != -1 && q6.getCheckedRadioButtonId() != -1 && q7.getCheckedRadioButtonId() != -1){
+                    if(radioButton1.getText()=="Non" && radioButton2.getText()=="Non" && radioButton3.getText()=="Non" && radioButton4.getText()=="Non" && radioButton5.getText()=="Non" && radioButton6.getText()=="Non" && radioButton7.getText()=="Non"){
+                        Toast.makeText(QuestionActivity.this, "Vous ne pouvez pas....", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(),
+                                "1: "+radioButton1.getText()+
+                                        "\n2: "+radioButton2.getText()+
+                                        "\n3: "+ radioButton3.getText()+
+                                        "\n4: "+radioButton4.getText()+
+                                        "\n5: "+radioButton5.getText()+
+                                        "\n6: "+radioButton6.getText()+
+                                        "\n7: "+radioButton7.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(QuestionActivity.this, "Question oublié", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                overridePendingTransition(android.R.anim.slide_out_right, android.R.anim.slide_in_left);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
