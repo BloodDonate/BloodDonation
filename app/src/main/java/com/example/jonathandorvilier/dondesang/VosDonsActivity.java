e16fc734990c5929352213a1507ac515f2e42a3b
IngDjason <djason_sylvaince@yahoo.fr>
2017-09-11 02:28:55 -0400

    Vos dons et Vos demandes
    lister vos dons et demandes

--- app/src/main/java/com/example/jonathandorvilier/dondesang/VosDonsActivity.java
+++ app/src/main/java/com/example/jonathandorvilier/dondesang/VosDonsActivity.java
@@ -1,11 +1,37 @@
 package com.example.jonathandorvilier.dondesang;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.example.jonathandorvilier.dondesang.adapter.VosDonsAdapter;
+import com.example.jonathandorvilier.dondesang.model.VosDons;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class VosDonsActivity extends AppCompatActivity {
 
+    ArrayList<VosDons> dons;
+    VosDonsAdapter adapter;
+    ProgressBar progress;
+    ListView lvVosDons;
+    private SwipeRefreshLayout swiperefresh;
+    SharedPreferences sharedPreferences ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +41,29 @@ public class VosDonsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Vos Dons :");
+
+        sharedPreferences = getSharedPreferences("PreferencesTAG", Context.MODE_PRIVATE);
+
+        lvVosDons = (ListView) findViewById(R.id.lvDemande);
+        progress = (ProgressBar ) findViewById(R.id.progress);
+        swiperefresh = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+
+        swiperefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                progress.setVisibility(View.VISIBLE);
+                getData();
+                swiperefresh.setRefreshing(false);
+            }
+        });
+
+        // Configure the refreshing colors
+        swiperefresh.setColorSchemeResources(android.R.color.holo_red_light,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_red_dark);
+
+        progress.setVisibility(View.VISIBLE);
+        getData();
     }
 
     @Override
@@ -26,6 +75,43 @@ public class VosDonsActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void getData() {
+        //set adapter
+        dons = new ArrayList<>();
+        adapter = new VosDonsAdapter(getApplicationContext(), dons);
+        lvVosDons.setAdapter(adapter);
+
+        String url = "http://astruitier.com/blood_donation/liste_vos_dons.php";
+
+        AsyncHttpClient client = new AsyncHttpClient(true, 80, 443);
+        client.get(url, new JsonHttpResponseHandler(){
+
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray articleJsonResults = null;
+                try {
+                    articleJsonResults = response.getJSONArray("response");
+                    adapter.addAll(VosDons.fromJsonArray(articleJsonResults));
+                    adapter.notifyDataSetChanged();
+                    Log.d("Debug", dons.toString());
+                    progress.setVisibility(View.GONE);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    getData();
+                }
+            }
+
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("echec: ",responseString.toString() );
+                getData();
+            }
+        });
     }
 
 }
