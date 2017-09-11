6cb7a60cda51e18333c2b9fb226bdba3052ec2e0
unknown <jonathandorvilier@gmail.com>
2017-09-11 17:55:16 -0400

    changes

--- app/src/main/java/com/example/jonathandorvilier/dondesang/activity_Rules.java
+++ app/src/main/java/com/example/jonathandorvilier/dondesang/activity_Rules.java
@@ -2,13 +2,9 @@ package com.example.jonathandorvilier.dondesang;
 
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-=======
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
->>>>>>> f1345917c400dd2e3036ed019ba8c7a7b6f5722c
+import android.view.View;
 import android.widget.TextView;
 
 public class activity_Rules extends ActionBarActivity implements View.OnClickListener {
@@ -17,18 +13,22 @@ public class activity_Rules extends ActionBarActivity implements View.OnClickLis
     private TextView tvRepons1;
     private TextView tvRepons2;
     private TextView tvquestion2;
+    private TextView tvRepons3;
+    private TextView tvquestion3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__rules);
 
-<<<<<<< HEAD
+
         tvRulesText = (TextView) findViewById(R.id.tvRulesText);
         tvRepons1 = (TextView) findViewById(R.id.tvRepons1);
         tvRepons2 = (TextView) findViewById(R.id.tvRepons2);
         tvquestion2 = (TextView) findViewById(R.id.tvquestion2);
 
+        tvRepons3 = (TextView) findViewById(R.id.tvRepons3);
+        tvquestion3 = (TextView) findViewById(R.id.tvquestion3);
     }
 
 
@@ -40,7 +40,8 @@ public class activity_Rules extends ActionBarActivity implements View.OnClickLis
 
                 startActivity(new Intent(this, activity_Rules.class));
                 break;
-=======
+
+                    }
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -57,7 +58,7 @@ public class activity_Rules extends ActionBarActivity implements View.OnClickLis
                 overridePendingTransition(android.R.anim.slide_out_right, android.R.anim.slide_in_left);
             default:
                 return super.onOptionsItemSelected(item);
->>>>>>> f1345917c400dd2e3036ed019ba8c7a7b6f5722c
+
         }
     }
 }
